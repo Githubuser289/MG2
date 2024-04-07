f013efdeaf636aacc08d95fe293c93b16d7c80fa
Githubuser289 <133155777+Githubuser289@users.noreply.github.com>
2024-04-07 15:21:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="money-guard">
+      <BrowserRouter basename="MG2">
         <PersistGate loading={null} persistor={persistor}>
           <App />
           <ToastContainer
